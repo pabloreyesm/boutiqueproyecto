e12be6512bc4493a5684611c8155d5b7e6ebd25d
pabloreyesm <74833048+pabloreyesm@users.noreply.github.com>
2021-05-30 18:57:48 -0500

    Update EmpleadoController.java

--- src/main/java/com/dwa/boutique/controladores/EmpleadoController.java
+++ src/main/java/com/dwa/boutique/controladores/EmpleadoController.java
@@ -1,15 +1,16 @@
 package com.dwa.boutique.controladores;
 
-
-import org.springframework.web.bind.annotation.GetMapping;
+import java.util.Optional;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
-import java.util.Optional;
+
 
 
 import com.dwa.boutique.servicios.ServicioEmpleado;
